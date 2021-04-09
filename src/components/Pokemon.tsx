c276c44d31ce1e514c3878d973c411cb6da9aae1
Ricardo Aguilar <jraguilars@icloud.com>
2021-04-09 16:23:40 -0500

    custom breakpoints

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -86,8 +86,8 @@ const ShowPokemon: React.FC = () => {
 
     return (
         <>
-            <div className="content-center items-center mx-auto grid grid-cols-3 gap-4 my-8 max-w-7xl">
-                {pokemons && pokemons.slice(0, total).map((pokemon: any) => {
+            <div className="content-center items-center mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8 max-w-7xl">
+                {pokemons && pokemons.slice(0, total).map((pokemon: PokemonDetails) => {
                     return (
                         <PokeCard key={pokemon.id} details={pokemon}></PokeCard>
                     )
