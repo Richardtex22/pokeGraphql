26b5a38fb54c22c7b11972becc9e47ec8ad63bb1
Ricardo Aguilar <jraguilars@icloud.com>
2021-04-09 16:11:56 -0500

    changing card colors

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -16,19 +16,23 @@ type TDetails = {
     width: 100%;
     padding-top: 100%;
  */
+
+ const cardStyle = {
+    borderRadius: '15px 80px 80px 15px'
+ }
 const PokeCard: React.FC<TDetails> = ({ details }) => {
     const { image, id, name, number } = details;
     return (
         <>
-            <div className="relative w-80 h-auto flex flex-row justify-between justify-self-center rounded-lg bg-gradient-to-r from-red-600 to-red-50">
-                <div className="grid grid-flow-row auto-rows-max gap-4 justify-start p-4 text-left text-white rounded-l-lg">
+            <div className="relative w-80 h-auto flex flex-row justify-between justify-self-center bg-gradient-to-r from-gray-300 to-gray-100" style={cardStyle}>
+                <div className="grid grid-flow-row auto-rows-max gap-4 justify-start p-4 text-left text-gray-800 rounded-l-lg">
                     <p>{`#${number}`}</p>
                     <p>{name}</p>
                 </div>
-                <div className="w-1/2 opacity-60 hover:opacity-100 overflow-hidden">
+                <div className="w-1/2 hover:opacity-70 overflow-hidden">
                     <div className="relative w-42 h-40">
                     <img className="z-10 absolute h-full w-full object-fill rounded-full transition duration-1000 ease-in-out transform hover:-translate-y-1 hover:scale-0" src={image} alt="" />
-                    <img className="justify-center opacity-70 transform scale-50 -translate-x-2" src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/240px-Pokebola-pokeball-png-0.png' alt="poke" />
+                    <img className="justify-center opacity-90 transform scale-50 -translate-x-2" src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/240px-Pokebola-pokeball-png-0.png' alt="poke" />
                     </div>
                 </div>
             </div>
